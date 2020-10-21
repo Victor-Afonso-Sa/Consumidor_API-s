2746cf54716219f3c02334f8f28e10ed08c4c773
Victor <victorafonso@frwk.com.br>
2020-10-21 18:44:09 -0300

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { EMPTY } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
   title = 'front';
+  registros;
+  uriString = 'http://localhost:8081/lib/generic';
+  ngOnInit(): void {
+    this.http.get(this.uriString).subscribe((data) => (this.registros = data));
+  }
+  edit(obj) {
+    console.log('EDIÇÂO');
+    this.http
+      .put(this.uriString, obj, { params: { ID: obj['ID'] } })
+      .subscribe();
+  }
+  new(obj) {
+    console.log('NOVO');
+    let array = [];
+    array.push(obj);
+    this.http.post(this.uriString, array).subscribe();
+  }
+  excluir(obj) {
+    console.log('DELETE');
+    this.http.delete(this.uriString,{ params: { ID: obj['ID'] } }).subscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GenericListModule } from 'generic-list';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GenericListModule,
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
